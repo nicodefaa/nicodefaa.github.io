bcc809ac3c905886b8dc62dbab9f5aa31cbf7856
nicodefaa <nico.defaa@myy.haaga-helia.fi>
2024-09-24 12:57:15 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function toggleMenu() {
 }
 
 /* Close the menu if the user clicks outside of it */
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
         let dropdowns = document.getElementsByClassName("dropdown-content");
         for (let i = 0; i < dropdowns.length; i++) {
