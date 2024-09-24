333aba388653ca35173c20039efe8117462cfa36
nicodefaa <nico.defaa@myy.haaga-helia.fi>
2024-09-24 09:56:06 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+/* Close the menu if the user clicks outside of it */
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
